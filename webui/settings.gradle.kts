b7b1606500bd2a0f45f2a4d2a894edb04e7564de
Lotusify <trungdanghuu289@gmail.com>
2025-08-08 22:28:53 +0700

    Revert "remove :webui"
    This reverts commit b0bb41129c9ff45bd4638602fb0a0de85050ba89.

---
+++ webui/settings.gradle.kts
@@ -0,0 +1,19 @@
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+include(
+    ":ui",
+    ":ext",
+    ":platform",
+    ":hidden-api"
+)
